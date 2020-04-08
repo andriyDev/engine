e47ca00378fc46a0f68088258f1a0c9c64de7113
andriyDev <andriydzikh@gmail.com>
2020-04-08 19:30:59 -0400

    Setup the project with proper includes and libs and stuffs.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,35 @@
 
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <stdio.h>
 
 int main()
 {
-    printf("Hello World\n");
+    GLFWwindow* window;
+    if(!glfwInit())
+        return -1;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    window = glfwCreateWindow(1280, 720, "Title", NULL, NULL);
+    if(!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    glewExperimental = true;
+    if(glewInit() != GLEW_OK) {
+        return -1;
+    }
+
+    do {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    } while(glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
+
     return 0;
 }
