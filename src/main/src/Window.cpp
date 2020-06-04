e327bcfe51f3343f821b402615cb848876febb26
andriyDev <andriydzikh@gmail.com>
2020-06-04 18:21:14 -0400

    Fixed a small issue in how Window resize was handled. This helps us debug the UI system.

--- src/main/src/Window.cpp
+++ src/main/src/Window.cpp
@@ -13,9 +13,11 @@ Window::~Window()
 void Window::window_resized(GLFWwindow* gwindow, int width, int height)
 {
     Window* window = static_cast<Window*>(glfwGetWindowUserPointer(gwindow));
-    
+
     window->width = width;
     window->height = height;
+
+    glViewport(0, 0, width, height);
 }
 
 void Window::key_event(GLFWwindow* gwindow, int key, int scancode, int action, int mods)
@@ -105,7 +107,7 @@ void Window::build()
     glfwSetInputMode(window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
 
     glfwSetWindowUserPointer(window, this);
-    glfwSetWindowSizeCallback(window, Window::window_resized);
+    glfwSetFramebufferSizeCallback(window, Window::window_resized);
     glfwSetKeyCallback(window, Window::key_event);
     glfwSetCharCallback(window, Window::char_typed);
     glfwSetMouseButtonCallback(window, Window::mouse_btn_event);
